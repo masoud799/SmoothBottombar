5609f0f7ee09bb0f69aebe02f3a0fbc170fb5699
masoud mokhtari <masoud.mokhtari69@gmail.com>
2021-01-24 18:59:53 +0330

    We disable animation of bottomBar item at start.

--- lib/src/main/java/ir/magicmirror/android/libs/widgets/SmoothBottomBar.kt
+++ lib/src/main/java/ir/magicmirror/android/libs/widgets/SmoothBottomBar.kt
@@ -38,6 +38,8 @@ class SmoothBottomBar @JvmOverloads constructor(
     private var indicatorLocation = barSideMargins
     private val rect = RectF()
 
+    private var firstTime: Boolean = true
+
     private var items = listOf<BottomBarItem>()
 
     // Attribute Defaults
@@ -517,24 +519,31 @@ class SmoothBottomBar @JvmOverloads constructor(
                 }
             }
 
-            ValueAnimator.ofFloat(
-                indicatorLocation,
-                items[itemActiveIndex].rect.left
-            ).apply {
-                duration = itemAnimDuration
-                interpolator = DecelerateInterpolator()
-                addUpdateListener { animation ->
-                    indicatorLocation = animation.animatedValue as Float
+            if (firstTime) {
+                indicatorLocation = items[itemActiveIndex].rect.left
+                currentIconTint = itemIconTintActive
+                invalidate()
+                firstTime = false
+            } else {
+                ValueAnimator.ofFloat(
+                    indicatorLocation,
+                    items[itemActiveIndex].rect.left
+                ).apply {
+                    duration = itemAnimDuration
+                    interpolator = DecelerateInterpolator()
+                    addUpdateListener { animation ->
+                        indicatorLocation = animation.animatedValue as Float
+                    }
+                    start()
                 }
-                start()
-            }
 
-            ValueAnimator.ofObject(ArgbEvaluator(), itemIconTint, itemIconTintActive).apply {
-                duration = itemAnimDuration
-                addUpdateListener {
-                    currentIconTint = it.animatedValue as Int
+                ValueAnimator.ofObject(ArgbEvaluator(), itemIconTint, itemIconTintActive).apply {
+                    duration = itemAnimDuration
+                    addUpdateListener {
+                        currentIconTint = it.animatedValue as Int
+                    }
+                    start()
                 }
-                start()
             }
         }
     }
